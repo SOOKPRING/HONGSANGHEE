15d36b3b6f49b9ce26e7598c3f68157c41cc6621
SangHee-Hong <hongdabagi@gmail.com>
2025-03-24 02:09:42 +0900

    [Chore]4:DI를 이용한 같은 레포지토리로 한 인스턴스를 생성하도록 변경

---
+++ hello-spring/hello-spring/src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -0,0 +1,10 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+
+}
